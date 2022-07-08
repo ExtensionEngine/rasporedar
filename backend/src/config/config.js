3483684dffef5d621b9c82a882a211897f3062b2
Bartol Deak <bdeak@extensionengine.com>
2022-07-08 12:15:51 +0200

    setup sequelize

---
+++ backend/src/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    username: 'rasporedar',
+    password: 'rasporedar',
+    database: 'rasporedar',
+    host: 'localhost',
+    port: 3002,
+    dialect: 'postgres',
+  },
+};
