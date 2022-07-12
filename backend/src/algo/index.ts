1838ad5a16fe03897b78855b88a3b353cf42d357
Bartol Deak <bdeak@extensionengine.com>
2022-07-12 16:26:36 +0200

    get lists of excluded subject ids

--- backend/src/algo/index.ts
+++ backend/src/algo/index.ts
@@ -1,7 +1,13 @@
 import { GenerateScheduleProps, GenerateScheduleResult } from './types';
+import { getExcludedSubjectIndexes } from './utils';
 
-function generateSchedule(props: GenerateScheduleProps): GenerateScheduleResult {
-  return { classes: [{ name: '' }], teachers: [{ name: '' }] };
+export function generateSchedule({ classes, classrooms }: GenerateScheduleProps): GenerateScheduleResult {
+  const subjects = classes.map(c => c.subjects).flat();
+
+  const subjectIndexesExcludedByTeacher = getExcludedSubjectIndexes(subjects, s => s.teacher?.name);
+  const subjectIndexesExcludedByClassroom = getExcludedSubjectIndexes(subjects, s => s.classroom?.name);
+
+  console.log(subjectIndexesExcludedByTeacher, subjectIndexesExcludedByClassroom);
+
+  return { classes: [], teachers: [] };
 }
-
-export { generateSchedule };
