e3e0cf16f156374795f130404992038cbbb2690a
Nikola Kelava <nikola.kelava94@gmail.com>
2022-07-20 10:36:02 +0200

    Remove Swagger configuration

--- backend/src/routes/teacher.ts
+++ backend/src/routes/teacher.ts
@@ -3,15 +3,6 @@ import Teacher from 'models/teacher';
 
 const router = Router();
 
-/**
- * @openapi
- * /teachers:
- *   get:
- *     description: Get all teachers
- *     responses:
- *       200:
- *         description: Returns all teachers.
- */
 router.get('/', async (req: Request, res: Response) => {
   const teachers = await Teacher.findAll();
 
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,15 +3,6 @@ import User from 'models/user';
 
 const router = Router();
 
-/**
- * @openapi
- * /users:
- *   get:
- *     description: Get all users
- *     responses:
- *       200:
- *         description: Returns all users.
- */
 router.get('/', async (req: Request, res: Response) => {
   const users = await User.findAll();
 
