8aef058166927988bd05b8ffed4a7aa49d73eaa9
Bartol Deak <bdeak@extensionengine.com>
2022-08-11 13:54:11 +0200

    parse timetable hash keys

--- backend/src/routes/timetable.ts
+++ backend/src/routes/timetable.ts
@@ -5,7 +5,11 @@ import { generateTimetableProps } from 'algorithm/seed';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.json(generateTimetable(generateTimetableProps));
+  const { timetable } = generateTimetable(generateTimetableProps);
+
+  res.json({
+    timetable: Object.fromEntries(Object.keys(timetable).map(key => [JSON.parse(key), timetable[key]])),
+  });
 });
 
 export default router;
