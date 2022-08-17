663d8e293d9fc9dd863a38b3a8370a7082e2ee20
Nikola Kelava <nikola.kelava94@gmail.com>
2022-08-17 23:49:12 +0200

    Add timetable generator view

---
+++ frontend/src/views/TimetableGeneratorView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ConstraintForm from '@/components/ConstraintForm/ConstraintForm.vue';
+</script>
+
+<template>
+  <div class="container">
+    <constraint-form />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100vw;
+  margin: 10%;
+}
+</style>
