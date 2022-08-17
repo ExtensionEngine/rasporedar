b5a9ad754f355bd9b5b73e61ec26cbcf4c546825
Nikola Kelava <nikola.kelava94@gmail.com>
2022-08-17 16:26:52 +0200

    Add menu link text

--- frontend/src/components/SideBar/SideBar.vue
+++ frontend/src/components/SideBar/SideBar.vue
@@ -1,11 +1,51 @@
 <script setup>
-import logoURL from '@/assets/img/rasporedar_logo.svg';
+import ClassroomIcon from '@/assets/img/sidebar/classroom_icon.svg';
+import LogoURL from '@/assets/img/rasporedar_logo.svg';
+import ProfileIcon from '@/assets/img/sidebar/profile_icon.svg';
+import TeacherIcon from '@/assets/img/sidebar/teacher_icon.svg';
+import TimetableAddIcon from '@/assets/img/sidebar/timetable_add_icon.svg';
+import TimetableIcon from '@/assets/img/sidebar/timetable_icon.svg';
 </script>
 
 <template>
   <aside class="sidebar">
     <div class="logo">
-      <img :src="logoURL" alt="Vue" />
+      <img :src="LogoURL" alt="Vue" />
+    </div>
+
+    <h4>Menu</h4>
+    <hr class="sidebar-line" />
+    <div class="menu">
+      <div class="link">
+        <span class="icon-wrapper">
+          <img class="icon" :src="ProfileIcon" />
+        </span>
+        <span class="link-text"> My Profile </span>
+      </div>
+      <div class="link">
+        <span class="icon-wrapper">
+          <img class="icon" :src="ClassroomIcon" />
+        </span>
+        <span class="link-text"> Classrooms </span>
+      </div>
+      <div class="link">
+        <span class="icon-wrapper">
+          <img class="icon" :src="TeacherIcon" />
+        </span>
+        <span class="link-text"> Teachers </span>
+      </div>
+      <div class="link">
+        <span class="icon-wrapper">
+          <img class="icon" :src="TimetableAddIcon" />
+        </span>
+        <span class="link-text"> Timetable Generator </span>
+      </div>
+      <div class="link">
+        <span class="icon-wrapper">
+          <img class="icon" :src="TimetableIcon" />
+        </span>
+        <span class="link-text"> Timetables </span>
+      </div>
     </div>
   </aside>
 </template>
@@ -16,7 +56,7 @@ import logoURL from '@/assets/img/rasporedar_logo.svg';
   flex-direction: column;
   background-color: var(--color-darker);
   color: var(--color-light);
-  width: calc(3rem + 32px);
+  width: 300px;
   overflow: hidden;
   min-height: 100vh;
   padding: 1rem;
@@ -32,5 +72,49 @@ import logoURL from '@/assets/img/rasporedar_logo.svg';
 
 .logo img {
   width: 4rem;
+}
+
+h4 {
+  color: var(--color-muted);
+  margin-bottom: 0;
+}
+
+.sidebar-line {
+  color: var(--color-muted);
+  width: 100%;
+}
+
+.menu {
+  margin: 0 -1rem;
+}
+
+.link {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: 0.2s ease-in-out;
+  margin-left: 0.4rem;
+  padding: 0.5rem 1rem;
+}
+
+.link:hover {
+  background-color: var(--color-active);
+}
+
+.icon-wrapper {
+  font-size: 1em;
+  color: var(--color-light);
+}
+
+.icon {
+  width: 40px;
+  height: 40px;
+  margin-right: 1rem;
+}
+
+.link-text {
+  opacity: 1;
+  color: var(--color-light);
+  transition: opacity 0.3s ease-in-out;
 }
 </style>
