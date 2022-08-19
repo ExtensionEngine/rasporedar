df8a70447c760c9263eec56df083e25de8cc89f6
Nikola Kelava <nikola.kelava94@gmail.com>
2022-08-19 09:16:42 +0200

    Add init my profile files

---
+++ frontend/src/components/MyProfile/MyProfile.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="main">My Profile Page</div>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/views/MyProfileVIew.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MyProfile from '@/compoenents/MyProfile';
+</script>
+
+<template>
+  <div class="container">
+    <my-profile />
+  </div>
+</template>
+
+<style scoped></style>
