2c606ddbb98debefebedb128b6aa0d21f5cd897d
Nikola Kelava <nikola.kelava94@gmail.com>
2022-08-19 11:34:19 +0200

    Add profie illustration

--- frontend/src/components/MyProfile/MyProfile.vue
+++ frontend/src/components/MyProfile/MyProfile.vue
@@ -1,31 +1,41 @@
 <script setup>
 import DeleteIcon from '@/assets/img/delete_icon.svg';
+import ProfileIllustration from '@/assets/img/illustrations/profile_illustration.svg';
 import { useUserStore } from '@/stores/user';
 
 const store = useUserStore();
 </script>
 
 <template>
-  <div class="main">
+  <div class="main-wrapper">
     <h2>My Profile</h2>
     <hr />
-    <div class="info">
-      <p class="info-item">Email: {{ store.user.email }}</p>
-      <p class="info-item">Password: *******</p>
-      <div class="flex"></div>
-      <div class="delete-wrapper">
-        <img class="item-icon" :src="DeleteIcon" />
-        Delete your account
+    <div class="rsprd-body">
+      <div class="info">
+        <p class="info-item">Email: {{ store.user.email }}</p>
+        <p class="info-item">Password: *******</p>
+        <div class="flex"></div>
+        <div class="delete-wrapper">
+          <img class="delete-icon" :src="DeleteIcon" />
+          Delete your account
+        </div>
+      </div>
+      <div class="illustration-wrapper">
+        <img :src="ProfileIllustration" class="illustration" />
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.main {
+.main-wrapper {
   padding: 20px;
   background-color: var(--color-lighter);
   border-radius: 20px;
+}
+
+.rsprd-body {
+  display: flex;
 }
 
 .info {
@@ -42,9 +52,22 @@ const store = useUserStore();
   height: 100px;
 }
 
-.item-icon {
+.delete-icon {
   height: 20px;
   width: 20px;
   vertical-align: bottom;
+}
+
+.illustration-wrapper {
+  width: 50%;
+  position: relative;
+}
+
+.illustration {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 280px;
+  height: 280px;
 }
 </style>
