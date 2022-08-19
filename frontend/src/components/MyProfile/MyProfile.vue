b98d964437d365dab4db41ed2c0acde7220037e4
Nikola Kelava <nikola.kelava94@gmail.com>
2022-08-19 10:46:29 +0200

    Add profile info section

--- frontend/src/components/MyProfile/MyProfile.vue
+++ frontend/src/components/MyProfile/MyProfile.vue
@@ -1,7 +1,50 @@
-<script setup></script>
+<script setup>
+import DeleteIcon from '@/assets/img/delete_icon.svg';
+import { useUserStore } from '@/stores/user';
+
+const store = useUserStore();
+</script>
 
 <template>
-  <div class="main">My Profile Page</div>
+  <div class="main">
+    <h2>My Profile</h2>
+    <hr />
+    <div class="info">
+      <p class="info-item">Email: {{ store.user.email }}</p>
+      <p class="info-item">Password: *******</p>
+      <div class="flex"></div>
+      <div class="delete-wrapper">
+        <img class="item-icon" :src="DeleteIcon" />
+        Delete your account
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main {
+  padding: 20px;
+  background-color: var(--color-lighter);
+  border-radius: 20px;
+}
+
+.info {
+  padding: 8px;
+  width: 50%;
+}
+
+.info-item {
+  border-bottom: 1px solid var(--color-muted);
+  padding: 4px;
+}
+
+.flex {
+  height: 100px;
+}
+
+.item-icon {
+  height: 20px;
+  width: 20px;
+  vertical-align: bottom;
+}
+</style>
