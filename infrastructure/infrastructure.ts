a5924972238e8dca9c319342d35266930d607669
Bartol Deak <bdeak@extensionengine.com>
2022-08-24 12:48:34 +0200

    rename exported var

--- infrastructure/infrastructure.ts
+++ infrastructure/infrastructure.ts
@@ -1,6 +1,7 @@
+import * as pulumi from "@pulumi/pulumi";
 import { createSiteBucket } from "./s3/siteBucket";
 
 const siteBucket = createSiteBucket("rasporedar-frontend");
 
 export const bucketName = siteBucket.id;
-export const websiteUrl = siteBucket.websiteEndpoint;
+export const bucketEndpoint = pulumi.interpolate`http://${siteBucket.websiteEndpoint}`;
