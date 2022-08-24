7985c4fbc17953d1301d7fe54d3191e09c32e6fc
Nikola Kelava <nikola.kelava94@gmail.com>
2022-08-24 11:22:08 +0200

    Add teacher page init component and view files

---
+++ frontend/src/views/TeachersView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { TeacherForm, TeacherList } from '@/components/Teacher';
+</script>
+
+<template>
+  <div class="container">
+    <teacher-form />
+    <teacher-list />
+  </div>
+</template>
