6c19c2492d7a0cbf1e7545eabd507c470a33a405
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-21 12:43:55 +0200

    Modify sidebar link item text transition time

--- frontend/src/layouts/SideBar/SideBarLink.vue
+++ frontend/src/layouts/SideBar/SideBarLink.vue
@@ -42,7 +42,7 @@ const checkExpanded = computed(() => (props.isCollapsed ? 'item-text' : 'item-te
 .item-text {
   opacity: 0;
   color: var(--color-light);
-  transition: opacity 0.5s ease-in-out;
+  transition: opacity 0.2s ease-in-out;
 }
 .item-text:hover {
   color: var(--color-main);
